664f10366b924f343cdc7c63bf0ec5c0f8550f27
Ali Hassan <alihassanuog50@gmail.com>
2024-05-26 16:40:54 +0500

    fifth commit

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://e28c9ca245792b8d640a5cb43ec4a4aa@o4507322506543104.ingest.de.sentry.io/4507322514997328",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
@@ -15,5 +15,4 @@ Sentry.init({
 
   // uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: process.env.NODE_ENV === 'development',
-  
 });
